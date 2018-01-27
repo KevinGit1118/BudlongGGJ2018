ade805dc2a0c0e8cfe18c692cbb071d17937936c
KevinGit1118 <31171132+KevinGit1118@users.noreply.github.com>
2018-01-27 11:25:10 -0800

    Add general table

---
+++ Assets/_scripts/GeneralTable.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GeneralTable
+{
+    public enum Type
+    {
+        Red = 0,
+        Blue = 1,
+        Yellow = 2,
+        Purple = 3,
+        Green = 4,
+        Orange = 5,
+        TypeEnumMax = 6
+    }
+    
+    public static Type Combine(Type type1, Type type2)
+    {
+        Type result = type1;
+        if (type1 == Type.Red && type2 == Type.Blue)
+        {
+            result = Type.Purple;
+        }
+        else if (type1 == Type.Blue && type2 == Type.Yellow)
+        {
+            result = Type.Green;
+        }
+        else if (type1 == Type.Yellow && type2 == Type.Red)
+        {
+            result = Type.Orange;
+        }
+        return result;
+    }
+    
+    public static Type GetRandomType()
+    {
+        return (Type)Random.Range(0, (int)Type.TypeEnumMax);
+    }
+
+    public static Color GetColor(Type typeValue)
+    {
+        Color resultColor = Color.red;
+        switch(typeValue)
+        {
+            case Type.Red:
+                resultColor = Color.red;
+                break;
+            case Type.Blue:
+                resultColor = Color.blue;
+                break;
+            case Type.Yellow:
+                resultColor = Color.yellow;
+                break;
+            case Type.Purple:
+                resultColor = new Color(255, 0, 255);
+                break;
+            case Type.Green:
+                resultColor = Color.green;
+                break;
+            case Type.Orange:
+                resultColor = new Color(255, 128, 0);
+                break;
+            default:
+                break;
+        }
+        return resultColor;
+    }
+}

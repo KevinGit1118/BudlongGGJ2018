abd08e672224b90661fcde1c246a6f673667e1d5
KevinGit1118 <31171132+KevinGit1118@users.noreply.github.com>
2018-01-26 23:21:41 -0800

    hook tuning nex waypoint function to player

--- Assets/_scripts/Player.cs
+++ Assets/_scripts/Player.cs
@@ -38,7 +38,7 @@ public class Player : MonoBehaviour {
 
         if (Input.GetKeyDown(changeDirKey))
         {
-            Debug.Log("change waypoint direction");
+            startWaypoint.ChangeNextWaypoint();
         }
     }
 

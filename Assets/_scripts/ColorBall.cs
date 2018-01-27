531f3ed13498c3393721424ddab7f32e48905fff
YTOWO <ognaisdts@hotmail.com>
2018-01-26 23:03:23 -0800

    player. colorball

---
+++ Assets/_scripts/ColorBall.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColorBall : MonoBehaviour {
+
+    public MeshRenderer ballMeshrenderer = null;
+    private Color _ballcolor = Color.white; 
+    public Color BallColor
+    {
+        get { return _ballcolor; }
+        set { _ballcolor = value;
+            SetColor(value);
+        }
+    }
+
+    private void SetColor(Color color)
+    {
+        ballMeshrenderer.material.color = color;
+    }
+
+}
--- Assets/_scripts/Player.cs
+++ Assets/_scripts/Player.cs
@@ -3,16 +3,51 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Player : MonoBehaviour {
+    [Header("Input")]
+    public KeyCode fireKey = KeyCode.A;
+    public KeyCode changeDirKey = KeyCode.S;
 
+    [Space(5)]
+    [Header("Color Ball")]
+    public Color ballColor = Color.red;
+    public float ballSpeed = 1;
+    public GameObject colorBallRef = null;
+
+    [Space(5)]
+    [Header("Waypoint")]
     public Waypoint startWaypoint = null;
+    
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    void Awake()
+    {
+        init();
+    }
+
+    void init()
+    {
+        //keep and do something
+    }
+
+    // Update is called once per frame
+    void Update () {
+        //catch input information
+        if (Input.GetKeyDown(fireKey))
+        {
+            FireColorBall();
+        }
+
+        if (Input.GetKeyDown(changeDirKey))
+        {
+            Debug.Log("change waypoint direction");
+        }
+    }
+
+    void FireColorBall()
+    {
+        GameObject colorballGO = Instantiate(colorBallRef);
+        colorballGO.transform.position = this.transform.position;
+        colorballGO.GetComponent<ColorBall>().BallColor = ballColor;
+    }
+
+
 }

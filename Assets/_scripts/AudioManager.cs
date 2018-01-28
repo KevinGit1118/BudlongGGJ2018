442ffc9f32b44c2c36a064ea9bd7e06cef0f3709
YTOWO <ognaisdts@hotmail.com>
2018-01-27 17:30:13 -0800

    audio

---
+++ Assets/_scripts/AudioManager.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class AudioManager : MonoBehaviour {
+
+    public AudioMixerSnapshot NormalSpeed;
+    public AudioMixerSnapshot GameOver;
+    public AudioMixerSnapshot SpeedUp;
+
+
+	// Use this for initialization
+	void Start () {
+        GamePlayManager.OnGameStart += OnGameStart;
+        GamePlayManager.OnGameOver += OnGameOver;
+        GamePlayManager.OnBackToMainMenu += OnBackToMainMenu;
+
+    }
+
+    void OnGameStart()
+    {
+        NormalSpeed.TransitionTo(0.01f);
+
+    }
+    void OnGameOver()
+    {
+        GameOver.TransitionTo(0.01f);
+    }
+
+    void OnBackToMainMenu()
+    {
+        NormalSpeed.TransitionTo(0.01f);
+    }
+
+
+}
